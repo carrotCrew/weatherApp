358fc382b75bdca90b2ed7796f88079fde691f20
Hanna Domagała <domagala.h@gmail.com>
2019-04-09 23:44:04 +0200

    Spinner added.

--- src/changeCity.js
+++ src/changeCity.js
@@ -1,6 +1,14 @@
 //MODUŁ DO ZMIANY MIASTA
 import { updateWeather, firstCapital } from './update';
 
+const showSpinner = (cityContainer) => {
+    cityContainer.innerHTML += ` <i class="fas fa-spinner"></i>`;
+}
+
+const hideSpinner = () => {
+    document.querySelector('.fa-spinner').remove();
+}
+
 const getCities = async () => {
     const response = await fetch(`https://raw.githubusercontent.com/carrotCrew/weatherApp/functionality/src/city.list.json`);
     const cityList = await response.json();
@@ -14,16 +22,19 @@ const correctPolishLetters = (string) => {
 
 const changeCity = async (key, containers, date) => {
     try {
-        const cityList = await getCities();
         const choosenCity = prompt('Podaj miasto:', 'Wrocław');
-        const { cityContainer } = containers;
-        const correctChoosenCity = correctPolishLetters(choosenCity.toLowerCase());
-        
         if(!choosenCity){
-        alert(`Należy podać nazwę miasta!`);
+            alert(`Należy podać nazwę miasta!`);
             return
         }
+        
+        const correctChoosenCity = correctPolishLetters(choosenCity.toLowerCase());
+        
+        const { cityContainer } = containers;
+        showSpinner(cityContainer);
 
+        const cityList = await getCities();
+        
         const cityArr = [];
         let cityId;
         let cityName;
@@ -41,6 +52,7 @@ const changeCity = async (key, containers, date) => {
  
         if (!cityArr.length) {
             alert(`Niestety, w naszej bazie nie ma miasta o nazwie: ${choosenCity}.`);
+            hideSpinner();
             return
         }
 
